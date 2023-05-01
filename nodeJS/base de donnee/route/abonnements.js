4d27f97c8cb8101cd93318ded67946382ffa64bd
Lisa_Dimitrief-Dontcheff <lisou-lisa_diana@hotmail.fr>
2023-05-01 23:21:39 +0200

    lorem

--- nodeJS/base de donnee/route/abonnements.js
+++ nodeJS/base de donnee/route/abonnements.js
@@ -47,31 +47,74 @@ const path = (app)=>{
         });
     });
 
-    app.patch('/abonnements/:id', (req,res)=>{
+    app.patch('/abonnements/:id/:value', (req,res)=>{
+        const abonnements_id = req.params.id;
+        let value = {};
+
+        if (req.params.value === 'nom') {
+            reqSQL = 'update abonnements set nom="?" where abonnements_id = ?';
+            value = nom
+        } else if (req.params.value === 'tarif'){
+            reqSQL = 'update abonnements set tarif=? where abonnements_id = ?';
+            value = tarif
+        }else if (req.params.value === 'hygiene'){
+            reqSQL = 'update abonnements set hygiene=? where abonnements_id = ?';
+            value = hygiene
+        }
+        else {
+            reqSQL = "update abonnements set coach=? where abonnements_id = ?";
+            value = coach
+        }
+        connection.query(reqSQL, [value, abonnements_id],(error, data)=>{
+            if (error) {
+                console.error(error);
+                res.status(500).send('erreur', {error});
+            } else {
+                res.status(201).send("C'est fait !");
+            }
+        })
+    });
+    app.put('/abonnements/:id', (req,res)=>{
         const nom = req.body.nom;
         const tarif = req.body.tarif;
         const hygiene = req.body.hygiene;
         const coach = req.body.coach;
         let requeteSQL ='update abonnements set nom="?", tarif=?, hygiene=?, coach=?  where abonnements_id = ?';
         req.getConnection((error, connection)=>{
-                if (error) {
-                    console.error(error);
-                    res.status(500).render('erreur', {error});
-                } else {
-                    connection.query(
-                        requeteSQL,[nom, tarif, hygiene, coach],
-                        (error, data)=>{
-                            if (error) {
-                                console.error(error);
-                                res.status(500).send('erreur', {error});
-                            } else {
-                                res.status(201).send('ok');
-                            }
-                        })
-                }
-        })
+            if (error) {
+                console.error(error);
+                res.status(500).render('erreur', {error});
+            } else {
+                connection.query(
+                    requeteSQL,[nom, tarif, hygiene, coach],
+                    (error, data)=>{
+                        if (error) {
+                            console.error(error);
+                            res.status(500).send('erreur', {error});
+                        } else {
+                            res.status(201).send("C'est fait !");
+                        }
+                    })
+            }
+        });
     });
 };
+
+                
+                    
+                        // requeteSQL,[nom, tarif, hygiene, coach],
+                        // (error, data)=>{
+                        //     if (error) {
+                        //         console.error(error);
+                        //         res.status(500).send('erreur', {error});
+                        //     } else {
+                        //         res.status(201).send('ok');
+                        //     }
+                        // })
+                
+        
+    
+
 
 
 
--- nodeJS/base de donnee/route/membre.js
+++ nodeJS/base de donnee/route/membre.js
@@ -2,13 +2,7 @@ const {connection} = require('../server');
 
 const path = (app)=>{
     app.get('/membre', (req,res)=>{
-    connection.query('select * from membre;',[], (error,results)=>{
-        if (error) throw error;
-            res.json(results)
-        })
-    });
-    app.get('/membrejoinclub', (req,res)=>{
-    connection.query('select membre.nom as nom, club.nom as club from membre join club using(club_id);',[], (error,results)=>{
+    connection.query('select membre.nom as nom, club.nom as club, date_naissance as "Date de Naissance", membre.mail, abonnements.nom as "Abonnement", statu.statu as "Status", medecin.nom as "Medecin"  from membre inner join club using(club_id) inner join abonnements using(abonnements_id) inner join statu using(statu_id) inner join medecin using(medecin_id);',[], (error,results)=>{
         if (error) throw error;
             res.json(results)
         })
@@ -16,7 +10,16 @@ const path = (app)=>{
     
     app.get('/membre/:id', (req,res)=>{
         const membre_id = req.params.id;
-    connection.query('select * from membre where membre_id=?;',[membre_id], (error,results)=>{
+    connection.query('select membre.nom as nom, club.nom as club, date_naissance as "Date de Naissance", membre.mail, abonnements.nom as "Abonnement", statu.statu as "Status", medecin.nom as "Medecin"  from membre inner join club using(club_id) inner join abonnements using(abonnements_id) inner join statu using(statu_id) inner join medecin using(medecin_id) where membre_id=?;',[membre_id], (error,results)=>{
+        if (error) throw error;
+            res.json(results)
+        })
+    });
+    app.get('/membre/:column/:value', (req,res)=>{
+        const column = req.params.column;
+        let value = req.params.value;
+        reqSQL = `select membre.nom as nom, club.nom as club, date_naissance as "Date de Naissance", membre.mail, abonnements.nom as "Abonnement", statu.statu as "Status", medecin.nom as "Medecin"  from membre inner join club using(club_id) inner join abonnements using(abonnements_id) inner join statu using(statu_id) inner join medecin using(medecin_id) where membre.${column} like '%${value}%'`;
+    connection.query(reqSQL,[], (error,results)=>{
         if (error) throw error;
             res.json(results)
         })
@@ -27,7 +30,7 @@ const path = (app)=>{
         const date_naissance = req.body.date_naissance;
         const tel = req.body.tel;
         const mail = req.body.mail;
-        const carte_identite = req.body.carte_identi;
+        const carte_identite = req.body.carte_identite;
         const cartif_medical = req.body.cartif_medical;
         const licence = req.body.licence;
         const login = req.body.login;
@@ -66,41 +69,69 @@ const path = (app)=>{
         });
     });
 
-    app.patch('/membre/:id', (req,res)=>{
+    app.patch('/membre/:id/:value', (req,res)=>{
+        const membre_id = req.params.id;
+        let value = {};
+
+        if (req.params.value === 'nom') {
+            reqSQL = 'update membre set nom="?" where membre_id=?';
+            value = nom
+        } else if (req.params.value === 'date_naissance'){
+            reqSQL = 'update membre set date_naissance=? where membre_id=?';
+            value = date_naissance
+        } else if (req.params.value === 'tel'){
+            reqSQL = 'update membre set tel="?" where membre_id=?';
+            value = tel
+        } else if (req.params.value === 'mail'){
+            reqSQL = 'update membre set mail="?" where membre_id=?';
+            value = mail
+        } else if (req.params.value === 'club_id'){
+            reqSQL = 'update membre set club_id=? where membre_id=?';
+            value = club_id
+        } else if (req.params.value === 'statu_id'){
+            reqSQL = 'update membre set statu_id=? where membre_id=?';
+            value = statu_id
+        }else if (req.params.value === 'abonnements_id'){
+            reqSQL = 'update membre se abonnements_id=? where membre_id=?';
+            value = abonnements_id
+        }
+        else {
+            reqSQL ='update membre set medecin_id=? where membre_id=?';
+            value = medecin_id
+        }
+        connection.query(reqSQL, [value, abonnements_id],(error, data)=>{
+            if (error) {
+                console.error(error);
+                res.status(500).send('erreur', {error});
+            } else {
+                res.status(201).send("C'est fait !");
+            }
+        });
+    });
+
+    app.put('/membre/:id', (req,res)=>{
         const nom = req.body.nom;
-        const date_naissance = req.body.date_naissance;
-        const tel = req.body.tel;
-        const mail = req.body.mail;
-        const carte_identite = req.body.carte_identi;
-        const cartif_medical = req.body.cartif_medical;
-        const licence = req.body.licence;
-        const login = req.body.login;
-        const mot_de_passe = req.body.mot_de_passe;
-        const club_id = req.body.club_id;
-        const statu_id = req.body.statu_id;
-        const abonnements_id = req.body.abonnements_id;
-        const carte_bleu = req.body.carte_bleu;
-        const medecin_id = req.body.medecin_id;
-        const assurance_id = req.body.assurance_id;
-        const adresse_id = req.body.adresse_id;
-        let requeteSQL ='update membre set nom=?, date_naissance=?, tel=?, mail=?, carte_identite=?, cartif_medical=?, licence =?, login=?, mot_de_passe=?, club_id=?, statu_id=?, abonnements_id=?, carte_bleu=?, medecin_id=?, assurance_id=?, adresse_id  where membre_id = ?';
+        const tarif = req.body.tarif;
+        const hygiene = req.body.hygiene;
+        const coach = req.body.coach;
+        let requeteSQL ='update membre set nom="?", date_naissance=?, tel="?", mail="?", club_id=?, statu_id=?, abonnements_id=?, medecin_id=? where membre_id=?';
         req.getConnection((error, connection)=>{
-                if (error) {
-                    console.error(error);
-                    res.status(500).render('erreur', {error});
-                } else {
-                    connection.query(
-                        requeteSQL,[nom, date_naissance, tel, mail, carte_identite, cartif_medical, licence , login, mot_de_passe, club_id, statu_id, abonnements_id, carte_bleu, medecin_id, assurance_id, adresse_id],
-                        (error, data)=>{
-                            if (error) {
-                                console.error(error);
-                                res.status(500).send('erreur', {error});
-                            } else {
-                                res.status(201).send('ok');
-                            }
-                        })
-                }
-        })
+            if (error) {
+                console.error(error);
+                res.status(500).render('erreur', {error});
+            } else {
+                connection.query(
+                    requeteSQL,[nom, tarif, hygiene, coach],
+                    (error, data)=>{
+                        if (error) {
+                            console.error(error);
+                            res.status(500).send('erreur', {error});
+                        } else {
+                            res.status(201).send("C'est fait !");
+                        }
+                    })
+            }
+        });
     });
 };
 
