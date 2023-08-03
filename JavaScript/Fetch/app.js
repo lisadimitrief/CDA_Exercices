0bb6ab0248be7406a35a45429691f342678ef599
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-08-03 10:42:05 +0200

    orga

--- JavaScript/Fetch/app.js
+++ JavaScript/Fetch/app.js
@@ -1,60 +1,57 @@
-// fetch("img/image.jpg")
-// .then(image => console.log(image));
+fetch("img/image.jpg")
+.then(image => console.log(image));
 
 
-// function fetchData() {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve('Data has been fetched!')
-//         }, 2000);
-//     })
-// }
+function fetchData() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('Data has been fetched!')
+        }, 2000);
+    })
+}
 
-// async function run() {
-//     try {
-//         let result = await fetchData()
-//         console.log(result);
-//     } catch (error){
-//         console.error(error);
-//     }
-// }
+async function run() {
+    try {
+        let result = await fetchData()
+        console.log(result);
+    } catch (error){
+        console.error(error);
+    }
+}
 
-// run()
+run()
 
-// async function fetchData() {
-//     try{
-//         const response = await fetch("https://jsonplaceholder.typicode.com/posts/")
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+async function fetchData() {
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/")
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-// fetchData()
+fetchData()
 
 // console.log('apres la fonction async');
 
 
 
 // I) Utilisez la méthode fetch pour récupérer un ficher texte à partir d'un serveur distant, puis affichez-le dans la console
-// fetch("https://jsonplaceholder.typicode.com/posts/")
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .catch(error => console.log(error));
+fetch("https://jsonplaceholder.typicode.com/posts/")
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
 
 
 // II) Écrivez une fonction ...
-// function getJSONData(url) {
-//     return fetch(url)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error =>console.log(error));
-// }
+function getJSONData(url) {
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error =>console.log(error));
+}
 
 // III)...
-// let image = fetch("img/image.jpg").then(response => console.log(response));
-// let zoneImg = document.createElement("img").setAttribute("src="+image+";")
-
-
-
+let image = fetch("img/image.jpg").then(response => console.log(response));
+let zoneImg = document.createElement("img").setAttribute("src="+image+";");
---
+++ JavaScript/liste de film/app.js
@@ -0,0 +1,34 @@
+let chiffre = [1,2,3,4,5,6,7,8,9,10];
+
+
+// for(i=0;i<chiffre.length;i++)
+// {
+//     // console.log("Table de "+(i+1))
+//     let table = document.createElement("p");
+//     table.append("Table de"+(i+1))
+//     document.body.append(table)
+//     for (let index=0; index<chiffre.length;index++){
+//         // console.log((index+1)+"x"+(1+i)+"="+(index+1)*(i+1))
+//         let calcul = document.createElement("p");
+//         calcul.append((index+1)+" x "+(1+i)+" = "+(index+1)*(i+1))
+//         document.body.append(calcul)
+//     }
+// }
+
+// document.body.style.display= "flex";
+// document.body.style.justifyContent= "space-evenly";
+// for(i=0;i<chiffre.length;i++)
+// {
+//     // console.log("Table de "+(i+1))
+//     let div = document.createElement("div")
+//     let h3 = document.createElement("h3");
+//     h3.append("Table de "+(i+1))
+//     document.body.append(div)
+//     div.append(h3)
+//     for (let index=0; index<chiffre.length;index++){
+//         // console.log((index+1)+"x"+(1+i)+"="+(index+1)*(i+1))
+//         let p = document.createElement("p");
+//         p.append((index+1)+" x "+(1+i)+" = "+(index+1)*(i+1))
+//         div.append(p)
+//     }
+// }
